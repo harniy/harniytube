7fc3aa19d5f83bdb716706a0bac93862baf0e929
harniy <forbedlam@gmail.com>
2020-08-25 21:18:52 +0300

    sorry

--- youtube_download/views.py
+++ youtube_download/views.py
@@ -5,8 +5,6 @@ from django.shortcuts import render
 from pytube import YouTube
 from youtube_dl import YoutubeDL
 
-
-
 url = ''
 
 
@@ -16,23 +14,25 @@ def ytb_down(request):
 
 def yt_download(request):
     global url
-    """метод request берет всю инфу со страницы, затем GET берет из формы input и get получает введенную инфу"""
-    url = request.GET.get('url')  # url это name='url' в форме input так назвал
-    """создаем объект, что бы узнать какое видео качать"""
-    yt = YouTube(url)
-    """создаем пустой список, куда будем добавлять элементы из цикла for"""
-    resolutions = []
-    """берем все полученные данные с url"""
-    strm_all = yt.streams.filter(progressive=True, file_extension='mp4').all()
-    for i in strm_all:
-        resolutions.append(i.resolution)
-    """метод dict.fromkeys нужен что бы удалить лишние значения из списка"""
-    resolutions = list(dict.fromkeys(resolutions))
-    embed_link = url.replace("watch?v=", "embed/")
-    print(resolutions)
-    print((embed_link))
-    return render(request, 'yt_download.html', {'rsl': resolutions, 'embd': embed_link})
-
+    try:
+        """метод request берет всю инфу со страницы, затем GET берет из формы input и get получает введенную инфу"""
+        url = request.GET.get('url')  # url это name='url' в форме input так назвал
+        """создаем объект, что бы узнать какое видео качать"""
+        yt = YouTube(url)
+        """создаем пустой список, куда будем добавлять элементы из цикла for"""
+        resolutions = []
+        """берем все полученные данные с url"""
+        strm_all = yt.streams.filter(progressive=True, file_extension='mp4').all()
+        for i in strm_all:
+            resolutions.append(i.resolution)
+        """метод dict.fromkeys нужен что бы удалить лишние значения из списка"""
+        resolutions = list(dict.fromkeys(resolutions))
+        embed_link = url.replace("watch?v=", "embed/")
+        print(resolutions)
+        print((embed_link))
+        return render(request, 'yt_download.html', {'rsl': resolutions, 'embd': embed_link})
+    except:
+        return render(request, 'sorry.html')
 
 
 def download_complete(request, res):
@@ -48,10 +48,8 @@ def download_complete(request, res):
             media_url = r['formats'][-1]['url']
         return render(request, 'download_complete.html', {'media_url': media_url})
     else:
-        pass
-
+        return render(request, 'sorry.html')
 
 
 def sorry(request):
     return render(request, 'sorry.html')
-
